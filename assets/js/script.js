382bedae8a21abeed1051dc1dc2e5b7439e5a607
Erica Siegel <siegel.erica@gmail.com>
2020-11-01 10:24:07 -0800

    Added some html and quiz questions

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+let counter = 60;
+
+
+
+let questions = [
+    {
+        q: "What is not considered a comonly used data type?",
+        c: ["booleans", "strings", "conditions", "numbers"],
+        a: "conditions"
+    },
+    {
+        q: "What keyword do you use to declare a variable?",
+        c: ["var", "if", "else", "return"],
+        a: "var"
+    },
+    {
+        q: "The condition of an if else statment is enclosed with?",
+        c: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        a: "parenthesis"
+    },
+    {
+        q: "Arrays in javaScript can be used to store what?",
+        c: ["booleans", "strings", "other arrays", "all of the above"],
+        a: "all of the above"
+    },
+    {
+        q: "what is the value of a boolean type?",
+        c: ["true", "false", "true and false", "0"],
+        a: "true and false"
+    },
+    {
+        q: "A very useful tool used during development and debuggin for printing content to the debugger is:",
+        c: ["JavaScript", "Terminal", "console.log", "data terminal"],
+        a: "console.log"
+    },
+
+]
