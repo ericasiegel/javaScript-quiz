c0775ed8d81d09e5bff43281e12c242c5016ab66
Erica Siegel <siegel.erica@gmail.com>
2020-11-01 13:02:41 -0800

    added comments and structure skeleton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
+// global variables
 let counter = 60;
+let quizEl = document.querySelector("#quiz");
+let answerEl = document.querySelector("#answers");
+let timerEl = document.querySelector("#timer");
 
 
-
+// quiz questions
 let questions = [
     {
         q: "What is not considered a comonly used data type?",
@@ -35,3 +39,19 @@ let questions = [
     },
 
 ]
+
+// create countdown for start of quiz button
+
+// create for loop for randomization of quiz questions and answers
+
+    //add buttons to C: elements
+    // if wrong answer is selected display it under next question and subtract 10 seconds
+    //when the answers are clicked they move on to another question
+
+// submit button enters results into result page where we can add our name and log it into the database
+
+// add a call to the database to show high scores
+
+
+// eventListener buttons refrencing each button to return to a different div
+
