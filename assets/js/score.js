a8e10ac4145390f867f53f906b230e85ec5a4792
Erica Siegel <siegel.erica@gmail.com>
2020-11-05 02:47:41 -0800

    cleanup

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,6 +1,6 @@
 let restartBtn = document.querySelector("button.restartBtn"),
     topScores = JSON.parse(localStorage.getItem("topScores") || "[]"),
-    scoreList = document.getElementById("userScores");
+    usersScores = document.getElementById("userScores");
 
 // sort scores
 topScores.sort(function (a, b) {
@@ -11,10 +11,8 @@ topScores.sort(function (a, b) {
 for (var s = 0; s < topScores.length; s++) {
     var newLi = document.createElement("li")
     newLi.textContent = topScores[s].name + " - " + topScores[s].score
-    scoreList.appendChild(newLi)
+    usersScores.appendChild(newLi)
 }
-
-
 // back to main page
 
 restartBtn.addEventListener("click", function () {
